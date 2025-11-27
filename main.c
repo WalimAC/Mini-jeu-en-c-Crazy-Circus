a7edb54d7da900a839a0f4f0b88313bdd3bb0d65
Walim AMOR-CHELIHI <amorwalim.pro@gmail.com>
2025-11-27 17:49:57 +0100

    Commencement du projet

---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by Walim Amor chelihi on 27/11/2025.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("\n⌈ -------------------------- ⌉\n| Commencement du projet...  |\n| Le 27/03/2025              |\n| Walim AMOR-CHELIHI         |\n⌊ -------------------------- ⌋\n");
+}
